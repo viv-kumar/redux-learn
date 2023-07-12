083abf9cdada74063022298bc00255c3dd8ea946
viv007084 <vivekguptatekari@gamil.com>
2023-07-12 14:15:09 +0530

    redux_learn completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useSelector } from "react-redux";
+import "./App.css";
+import Account from "./components/Account";
+import Bonus from "./components/Bonus";
 function App() {
+  const amount = useSelector((state) => state.account.amount);
+  const points = useSelector((state) => state.bonus.points);
+  const account = useSelector((state) => state.account);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>App</h4>
+      {account.pending ? (
+        <p>loading....</p>
+      ) : account.error ? (
+        <p>{account.error}</p>
+      ) : (
+        <h3>Current Amount : {amount} </h3>
+      )}
+      <h3>Total Bonus : {points}</h3>
+
+      <Account></Account>
+      <Bonus></Bonus>
     </div>
   );
 }
